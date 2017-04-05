1862a195f328fecd51ffb77a338e92a312b36bc5
Brian O'Reilly <fade@deepsky.com>
2017-04-05 15:49:50 -0400

    Yeah, I was right the first time.

--- t/cl-anagrams.lisp
+++ t/cl-anagrams.lisp
@@ -11,8 +11,8 @@
 ;;; tests assume the default wordlist.
 (plan nil)
 
-(is (length *wordlist*) 416111)
-(is (length (rutils:hash-table-keys *anagrams*)) 357027)
-(is (length (return-valid-anagrams *anagrams*)) 37775o)
+(ok (= (length *wordlist*) 416111))
+(ok (= (length (rutils:hash-table-keys *anagrams*)) 357027))
+(ok (= (length (return-valid-anagrams *anagrams*)) 37775o))
 
 (finalize)
