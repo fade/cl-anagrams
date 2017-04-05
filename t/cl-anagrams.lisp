094c9d3e0fcb82459ee31f5ae48155361f10bd7c
Brian O'Reilly <fade@deepsky.com>
2017-04-05 15:44:33 -0400

    let's see what happens with clisp.

--- t/cl-anagrams.lisp
+++ t/cl-anagrams.lisp
@@ -6,12 +6,13 @@
 
 (in-package :cl-anagrams-test)
 
-(plan nil)
 
 
 ;;; tests assume the default wordlist.
-(is (= (length *wordlist*) 416111))
-(is (= (length (rutils:hash-table-keys *anagrams*)) 357027))
-(is (= (length (return-valid-anagrams *anagrams*)) 37775))
+(plan nil)
+
+(is (length *wordlist*) 416111)
+(is (length (rutils:hash-table-keys *anagrams*)) 357027)
+(is (length (return-valid-anagrams *anagrams*)) 37775o)
 
 (finalize)
